4a92989837acbd273ad1f36671fa0e49f42af072
skaaadiii <xyanksat@gmail.com>
2025-03-18 11:46:12 +0800

    Site updated: 2025-03-18 11:46:11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1742265308907";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1742269538038";const preCache = ["/skaaadiii/images/taichi.png","/skaaadiii/images/banner.webp","/skaaadiii/css/loader.css","/skaaadiii/css/style.css","/skaaadiii/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
